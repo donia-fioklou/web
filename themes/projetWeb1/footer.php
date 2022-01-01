84f476d26a09edbafdfba192f3efd6f3618670f0
donia-fioklou <fiokloudonia3@gmail.com>
2022-01-01 19:02:09 +0000

    initial

---
+++ themes/projetWeb1/footer.php
@@ -0,0 +1,14 @@
+<footer>
+  <h2 id="contact">CONTACT</h2>
+  <table>
+    <tr> 
+      <td>59 rue de la kozah Nyékonakpoè</td>
+      <td>   07 BP:12456 Lomé 07,Togo</td>
+    </tr>
+    <tr> 
+      <td>iaitogo@iai-togo.tg</td>
+      <td>(00228) 22 20 47 00</td>
+    </tr> 
+  </table>
+  <marquee>Copyright &copy; FD Technologie 2021 - All Right Reserved</marquee>
+</footer>
---
+++ themes/projetWeb1/header.php
@@ -0,0 +1,32 @@
+  <div class="navigation w-100">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark p-0">
+        <a class="navbar-brand" href="index.html"><img src="images/logo.jpeg" alt="logo"></a>
+        <button class="navbar-toggler rounded-0" type="button" data-toggle="collapse" data-target="#navigation"
+          aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navigation">
+          <ul class="navbar-nav ml-auto text-center">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">Accueil</a>
+            </li>
+            <li class="nav-item @@about">
+              <a class="nav-link" href="about.html">Communiqué</a>
+            </li>
+            <li class="nav-item @@courses">
+              <a class="nav-link" href="courses.html">S'inscrire</a>
+            </li>
+            <li class="nav-item @@events">
+              <a class="nav-link" href="events.html">Se connecté</a>
+            </li>
+            
+            <li class="nav-item @@contact">
+              <a class="nav-link" href="contact.html">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
